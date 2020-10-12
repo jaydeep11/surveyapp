4269c7417241d86efb389819073003eb8011a606
jaydeep11 <jaydeep112000@gmail.com>
2020-10-12 18:39:24 +0530

    modified settings file

--- app/app/settings.py
+++ app/app/settings.py
@@ -140,7 +140,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     #Allow all host headers
-    ALLOWED_HOSTS = ['jops007.herokuapp.com']
+    ALLOWED_HOSTS = ['toddlejops.herokuapp.com']
     DEBUG = False
 
     #Static asset configuration
